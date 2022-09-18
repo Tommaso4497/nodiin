b4a83ae4ca8a2a1ae5a5a49113f835635007ec09
Tommaso4497 <ditusatommaso@gmail.com>
2022-09-18 19:33:07 +0200

    add icon and contact us

--- pages/components/Navigation.js
+++ pages/components/Navigation.js
@@ -9,13 +9,20 @@ import {
   Divider,
   Drawer,
   IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
   Toolbar,
   Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import InfoIcon from '@mui/icons-material/Info';
+import MailIcon from '@mui/icons-material/Mail';
+import CollectionsIcon from '@mui/icons-material/Collections';
 
 const Navigation = () => {
   const router = useRouter();
@@ -40,7 +47,6 @@ const Navigation = () => {
                 color="inherit"
                 onClick={handleDrawerOpen}
                 edge="start"
-                sx={{ mr: 2, ...(open && { display: "none" }) }}
               >
                 <MenuIcon />
               </IconButton>
@@ -52,6 +58,7 @@ const Navigation = () => {
               width: "240px",
               flexShrink: 0,
               "& .MuiDrawer-paper": {
+                backgroundColor: "#fefae0",
                 width: "240px",
                 boxSizing: "border-box",
               },
@@ -60,27 +67,63 @@ const Navigation = () => {
             anchor="left"
             open={open}
           >
-            <div>
+            <div >
               <IconButton onClick={handleDrawerClose}>
                 <KeyboardArrowLeftIcon />
               </IconButton>
               Menu
             </div>
             <Divider />
+            <List>
+              <ListItem>
+                <ListItemButton>
+                  <ListItemIcon><CollectionsIcon/></ListItemIcon>
+                  Chiacchierino</ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                  <ListItemIcon><CollectionsIcon/></ListItemIcon>
+                  Uncinetto</ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                <ListItemIcon><InfoIcon/></ListItemIcon>
+                  Informazioni</ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton>
+                  <ListItemIcon><MailIcon/></ListItemIcon>
+                  Contattaci</ListItemButton>
+              </ListItem>
+            </List>
           </Drawer>
         </>
       ) : (
-        <div style={{display:"flex",  backgroundColor: "#adc178", alignItems:"center", justifyContent:"space-between", width:"100%", padding:".25rem .5rem"}}>
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "#adc178",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+            padding: ".25rem .5rem",
+          }}
+        >
           <div>
             <Typography>Nodi in chiacchierino</Typography>
           </div>
-          <div style={{display:"flex",gap:".75rem", }}>
-            <IconButton><FacebookIcon/></IconButton>
-            <IconButton><InstagramIcon/></IconButton>
+          <div style={{ display: "flex", gap: ".75rem" }}>
+            <IconButton>
+              <FacebookIcon />
+            </IconButton>
+            <IconButton>
+              <InstagramIcon />
+            </IconButton>
             <Button variant="contained">Home</Button>
             <Button variant="contained">Chiacchierino</Button>
             <Button variant="contained">Uncinetto</Button>
             <Button variant="contained">Informazioni</Button>
+            <Button variant="contained">Contattaci</Button>
           </div>
         </div>
       )}
