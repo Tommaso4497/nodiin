9ba1efc90fb0a9989b7f15a32e80a4961ff3eb88
Tommaso di Tusa <tommaso4497@outlook.it>
2022-09-20 17:54:23 +0200

    insert footer and home page

--- pages/contactUs/contactUs.js
+++ pages/contactUs/contactUs.js
@@ -1,19 +1,36 @@
-import { TextField, Typography, Button } from "@mui/material";
+import { TextField, Typography, Button, useMediaQuery, IconButton } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import React from "react";
+import styles from "./contactUs.module.css";
+import theme from "../../styles/main";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 
 const contactUs = () => {
-  return (
-    <div style={{ padding: "1rem 2rem" }}>
-      <Typography variant="h5">Contattaci</Typography>
-      <Typography style={{ marginBottom: ".5rem" }}>Scrivici per qualsiasi informazione!</Typography>
-      <form >
-        <TextField label="Oggetto" variant="filled" size="small" fullWidth color="secondary"> </TextField>
-        <TextField style={{ marginTop: "1rem" }} minRows={15} multiline label="Contenuto" variant="filled" size="small" fullWidth color="secondary"> </TextField>
-        <Button style={{ marginTop: "1rem", gridColumn: "-2 / -1", placeSelf: "end" }} type="submit" variant="contained" endIcon={<SendIcon />}>Invia</Button>
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
+  return (
+    <div className={styles.contentWrapper}>
+      <Typography variant="h5">Contattaci</Typography>
+      <Typography className={styles.spacing} >Scrivici per qualsiasi informazione!</Typography>
+      <form className={styles.formWrapper} >
+        <TextField autoComplete="off" required label="Oggetto" variant="filled" size="small" fullWidth color="secondary"> </TextField>
+        <TextField autoComplete="off" required className={styles.spacing} minRows={15} multiline label="Contenuto" variant="filled" size="small" fullWidth color="secondary"> </TextField>
+        <Button className={styles.spacing} type="submit" variant="contained" endIcon={<SendIcon />}>Invia</Button>
       </form>
+
+      <div className={styles.footerTitle}>
+        <Typography>Oppure Contattaci tramite i nostri social</Typography>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+          <IconButton>
+            <FacebookIcon />
+          </IconButton>
+          <IconButton>
+            <InstagramIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import theme from "../styles/main";
+import Copertina from "../images/copertina.jpg";
+import Image from "next/image";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
@@ -10,7 +13,9 @@ export default function Home() {
         <meta name="description" content="Gioielli in pizzo chiacchierino" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <div >Home</div>
+      <Image src={Copertina} layout="responsive" />
+      <div style={{ textAlign: "center" }}>
+      </div>
     </div>
   );
 }
