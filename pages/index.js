c22364ac8ee5cbcc0760c4c267348fb1d4a66674
Tommaso4497 <ditusatommaso@gmail.com>
2022-09-22 15:23:49 +0200

    modified favicon

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,55 @@ import { Typography } from "@mui/material";
 export default function Home() {
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
-
   return (
     <div className={styles.container}>
       <Head>
         <title>Nodi in chiacchierino</title>
         <meta name="description" content="Gioielli in pizzo chiacchierino" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
-      <Typography variant="h4" style={{ fontStyle: "italic", marginTop: "1rem", textAlign: "center" }}>Ultime creazioni</Typography>
-      <div style={{ padding: "2rem", display: "grid", gridTemplateColumns: matches ? "1fr 1fr 1fr" : "1fr", gap: "4rem" }}>
-
+      <Typography
+        variant="h4"
+        style={{ fontStyle: "italic", marginTop: "1rem", textAlign: "center" }}
+      >
+        Ultime creazioni
+      </Typography>
+      <div
+        style={{
+          padding: "2rem",
+          display: "grid",
+          gridTemplateColumns: matches ? "1fr 1fr 1fr" : "1fr",
+          gap: "4rem",
+        }}
+      >
         {uncinettoElements.map((elem) => (
-          <Card key={elem.id} title={elem.title} image={elem.img} descr={elem.desc}></Card>
+          <Card
+            key={elem.id}
+            title={elem.title}
+            image={elem.img}
+            descr={elem.desc}
+          ></Card>
         ))}
-
       </div>
-    </div >
+    </div>
   );
 }
