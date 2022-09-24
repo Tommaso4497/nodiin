7dd86aae6ab1009e077177b3f906e8bb3bea8cce
Tommaso4497 <ditusatommaso@gmail.com>
2022-09-24 20:58:38 +0200

    fix contact page

--- pages/contactUs/index.js
+++ pages/contactUs/index.js
@@ -1,7 +1,7 @@
 import { TextField, Typography, Button, IconButton } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import React from "react";
-import styles from "../../styles/ContactUs.module.css";
+import styles from "./ContactUs.module.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import useMediaQuery from "@mui/material/useMediaQuery";
@@ -12,43 +12,60 @@ const ContactUs = () => {
   return (
     <div className={styles.contentWrapper}>
       <Typography variant="h5">Contattaci</Typography>
-      <Typography className={styles.spacing}>
-        Scrivici per qualsiasi informazione
-      </Typography>
-      <form className={styles.formWrapper}>
-        <TextField
-          autoComplete="off"
-          required
-          label="Oggetto"
-          variant="filled"
-          size="small"
-          fullWidth
-          color="secondary"
-        >
-        </TextField>
-        <TextField
-          autoComplete="off"
-          required
-          minRows={15}
-          multiline
-          label="Contenuto"
-          variant="filled"
-          size="small"
-          fullWidth
-          color="secondary"
-        ></TextField>
-        <Button
-          type="submit"
-          variant="contained"
-          endIcon={<SendIcon />}
-        >
-          Invia
-        </Button>
+      <Typography>Scrivici per qualsiasi informazione</Typography>
+      <form autoComplete="off">
+        <div className={styles.contentForm}>
+          <TextField
+            autoComplete="off"
+            required
+            label="Email"
+            variant="filled"
+            size="small"
+            fullWidth
+            color="secondary"
+          ></TextField>
+          <TextField
+            autoComplete="off"
+            required
+            label="Oggetto"
+            variant="filled"
+            size="small"
+            fullWidth
+            color="secondary"
+          ></TextField>
+          <TextField
+            autoComplete="off"
+            required
+            minRows={15}
+            multiline
+            label="Contenuto"
+            variant="filled"
+            size="small"
+            fullWidth
+            color="secondary"
+          ></TextField>
+        </div>
+        <div className={styles.wrapperButton} >
+          <Button
+            type="submit"
+            variant="contained"
+            endIcon={<SendIcon />}
+          >
+            Invia
+          </Button>
+        </div>
       </form>
 
-      <div className={styles.footerTitle}>
-        <Typography>Oppure contattaci tramite i nostri canali social</Typography>
-        <div style={{ display: "flex", justifyContent: matches ? "start" : "space-around" }}>
+      <div>
+        <Typography>
+          Oppure contattaci tramite i nostri  social
+        </Typography>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: matches ? "start" : "space-around",
+          }}
+        >
           <IconButton>
             <FacebookIcon fontSize="large" />
           </IconButton>
