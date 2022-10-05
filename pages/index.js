84b19ae1c15676f5d0ea80b6c15dd9235302c40c
Tommaso di Tusa <tommaso4497@outlook.it>
2022-10-05 16:24:53 +0200

    add carosel and section

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import React from "react";
 import { uncinettoElements } from "../utilsFunction/utilsFunction";
 import { Typography } from "@mui/material";
 import SingleCard from "./components/SingleCard";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Image from "next/image";
+
+import copertina from "../images/copertina.jpg";
+import { padding } from "@mui/system";
+
 
 export default function Home() {
 
@@ -38,8 +45,24 @@ export default function Home() {
         variant="h4"
         style={{ fontStyle: "italic", marginTop: "1rem", textAlign: "center" }}
       >
-        Ultime creazioni
+        Le mie ultime creazioni
       </Typography>
+      <div style={{ padding: "2rem" }}>
+        <Carousel autoPlay showArrows={false} showStatus={false} infiniteLoop dynamicHeight >
+          <div>
+            <Image src={copertina} />,
+          </div>
+          <div>
+            <Image src={copertina} />,
+          </div>
+          <div>
+            <Image src={copertina} />,
+          </div>
+        </Carousel>
+      </div>
+
+      <Typography style={{ backgroundColor: "#adc178", fontStyle: "italic", textAlign: "center" }} variant="h5"
+      >Collane in chiacchierino</Typography>
       <div className={styles.gridWrapper}>
         {uncinettoElements.map((elem) => (
           <SingleCard
@@ -50,6 +73,6 @@ export default function Home() {
           ></SingleCard>
         ))}
       </div>
-    </div>
+    </div >
   );
 }
