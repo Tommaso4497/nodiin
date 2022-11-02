e40f95d40a80d549e73ac4888b9e31db0b6f0c74
Tommaso di Tusa <tommaso4497@outlook.it>
2022-11-02 09:40:28 +0100

    fix useeffect import in chiacchierino section

--- pages/chiacchierino/index.js
+++ pages/chiacchierino/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import copertina from "../../images/copertina.jpg";
 import SearchIcon from "@mui/icons-material/Search";
