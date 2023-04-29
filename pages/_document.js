2a7b16f3e7c41ef410d231a6198ced6547bf4764
Tommaso4497 <ditusatommaso@gmail.com>
2023-04-29 12:27:26 +0200

    document restore

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default  class MyDocument extends Document {
-  render() {
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Italianno&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
-  }
+export default function Document() {
+    return (
+        <Html>
+            <Head >
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet"></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
-
-
