6f96eaf410cb8e4a13165ddc403d43600ec679b3
Tommaso4497 <ditusatommaso@gmail.com>
2023-04-29 12:20:39 +0200

    document fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-    return (
-        <Html>
-            <Head >
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet"></link>
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+export default  class MyDocument extends Document {
+  render() {
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Italianno&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>;
+  }
 }
+
+
