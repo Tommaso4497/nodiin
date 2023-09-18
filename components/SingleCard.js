296c1ab335c6597f753c36bd25658078adff92a7
Tommaso4497 <ditusatommaso@gmail.com>
2023-09-18 19:34:47 +0200

    delete description from single card

--- components/SingleCard.js
+++ components/SingleCard.js
@@ -5,24 +5,16 @@ import ImageEdit from "./ImageEdit";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import theme from "../styles/main";
 
-const SingleCard = ({ title, descr,image }) => {
+const SingleCard = ({ title, image }) => {
   const matches = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
-    <div>
       <div className={styles.card}>
         <div className={styles.image}>
           <ImageEdit url={image} />
         </div>
         <div className={styles.title}>{title}</div>
-        <Divider />
-        <div className={styles.descriptionWrapper}>{descr}</div>
-        <div className={styles.stats}>
-          <div className={styles.footerCard}>
-          </div>
-        </div>
       </div>
-    </div>
   );
 };
 
