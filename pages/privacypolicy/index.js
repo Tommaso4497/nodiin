02f82962c1e1b45ac4d1dcc28884598a28fbea3f
Tommaso4497 <ditusatommaso@gmail.com>
2023-09-29 20:12:13 +0200

    added privacy policy

--- pages/privacypolicy/index.js
+++ pages/privacypolicy/index.js
@@ -1,11 +1,121 @@
 import React from "react";
 
-const PrivacyPolicy = () =>{
-    return(
-        <div>
-            PrivacyPolicy
-        </div>
-    )
-}
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1 >
+        Privacy Policy
+        <br />
+        <span>Last Updated On 29-Sep-2023</span>
+        <br />
+        <span>Effective Date 29-Sep-2023</span>
+      </h1>
+
+      <p >
+        This Privacy Policy describes the policies of Nodi in Chiacchierino,
+        Campobasso, Campobasso 86021, Italy, email:
+        nodiinchiacchierino@gmail.com,
+        {/* phone: 3200980784 on the collection */}
+        , use
+        and disclosure of your information that we collect when you use our
+        website ( nodiinchiacchierino.it ). (the “Service”). By accessing or
+        using the Service, you are consenting to the collection, use and
+        disclosure of your information in accordance with this Privacy Policy.
+        If you do not consent to the same, please do not access or use the
+        Service.
+      </p>
+
+      <p>
+        We may modify this Privacy Policy at any time without any prior notice
+        to you and will post the revised Privacy Policy on the Service. The
+        revised Policy will be effective 180 days from when the revised Policy
+        is posted in the Service and your continued access or use of the Service
+        after such time will constitute your acceptance of the revised Privacy
+        Policy. We therefore recommend that you periodically review this page.
+      </p>
+
+      <ol>
+        <li>
+          <h2>Your Rights:</h2>
+          <p >
+            Depending on the law that applies, you may have a right to access
+            and rectify or erase your personal data or receive a copy of your
+            personal data, restrict or object to the active processing of your
+            data, ask us to share (port) your personal information to another
+            entity, withdraw any consent you provided to us to process your
+            data, a right to lodge a complaint with a statutory authority and
+            such other rights as may be relevant under applicable laws. To
+            exercise these rights, you can write to us at
+            nodiinchiacchierino@gmail.com. We will respond to your request in
+            accordance with applicable law.
+          </p>
+
+          <p>
+            Do note that if you do not allow us to collect or process the
+            required personal information or withdraw the consent to process the
+            same for the required purposes, you may not be able to access or use
+            the services for which your information was sought.
+          </p>
+        </li>
+        <li>
+          <h2>Cookies Etc.</h2>
+          <p>
+            To learn more about how we use these and your choices in relation to
+            these tracking technologies, please refer to our
+            <a href="nodiinchiacchierino.it/cookiepolicy">Cookie Policy.</a>
+          </p>
+        </li>
+
+        <li>
+          <h2>Security:</h2>
+          <p>
+            The security of your information is important to us and we will use
+            reasonable security measures to prevent the loss, misuse or
+            unauthorized alteration of your information under our control.
+            However, given the inherent risks, we cannot guarantee absolute
+            security and consequently, we cannot ensure or warrant the security
+            of any information you transmit to us and you do so at your own
+            risk.
+          </p>
+        </li>
+
+        <li>
+          <h2>Third Party Links & Use Of Your Information:</h2>
+          <p>
+            Our Service may contain links to other websites that are not
+            operated by us. This Privacy Policy does not address the privacy
+            policy and other practices of any third parties, including any third
+            party operating any website or service that may be accessible via a
+            link on the Service. We strongly advise you to review the privacy
+            policy of every site you visit. We have no control over and assume
+            no responsibility for the content, privacy policies or practices of
+            any third party sites or services.
+          </p>
+        </li>
+
+        <li>
+          <h2>Grievance / Data Protection Officer:</h2>
+          <p>
+            If you have any queries or concerns about the processing of your
+            information that is available with us, you may email our Grievance
+            Officer at Nodi in Chiacchierino, nodiinchiacchierino@gmail.com,
+            email: nodiinchiacchierino@gmail.com. We will address your concerns
+            in accordance with applicable law.
+          </p>
+        </li>
+      </ol>
+      <p>
+        Privacy Policy generated with{" "}
+        <a
+          target="_blank"
+          href="https://www.cookieyes.com/?utm_source=PP&utm_medium=footer&utm_campaign=UW"
+        >
+          CookieYes
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
 
 export default PrivacyPolicy;
