03b47b103cd8967f0272e242935378c1139abac5
Tommaso4497 <ditusatommaso@gmail.com>
2023-10-06 12:34:44 +0200

    added contact form

--- pages/contatti/index.js
+++ pages/contatti/index.js
@@ -1,21 +1,85 @@
-import { Input } from "@mui/material";
-import React from "react";
+import { Button, Input, TextField, TextareaAutosize } from "@mui/material";
+import React, { useState } from "react";
+import styles from "./Contatti.module.css";
 
 const Contatti = () => {
+  const [confirm, setConfirm] = useState(false);
   return (
     <div>
-      <form style={{display:"flex", justifyContent:"center",justifyContent:"center"}}>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Oggetto:
-          <input type="email" name="email" />
-        </label>
-        <textarea>Ciao</textarea>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className={styles.pageTitle}>
+        <p style={{ marginBlock: "0" }}>Contatti</p>
+      </div>
+      <p>Per qualsiasi curiosità, non esitare a contattarci!</p>
+      <div style={{ display: "grid", gridTemplateColumns: " 1fr 1fr" }}>
+        <div style={{ backgroundColor: "#f00" }}>
+          <Button>Coap</Button>
+        </div>
+        <div
+          style={{ backgroundColor: "#ff0", padding: "1rem 10rem 1rem 10rem" }}
+        >
+          <form>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+              }}
+            >
+              <TextField
+                label="Email"
+                type="email"
+                name="email"
+                variant="outlined"
+              />
+              <TextField
+                label="Oggetto"
+                type="text"
+                name="object"
+                variant="outlined"
+              />
+              <TextField
+                label="Oggetto"
+                type="text"
+                name="object"
+                multiline
+                minRows={20}
+                variant="outlined"
+              />
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <input
+                  onChange={() => {
+                    setConfirm(!confirm);
+                  }}
+                  type="checkbox"
+                />
+                <p>
+                  Ho letto e accetto la{" "}
+                  <a
+                    rel="noopener noreferrer"
+                    href="https://nodiinchiacchierino.it/privacypolicy/"
+                  >
+                    Privacy Policy
+                  </a>
+                </p>
+              </div>
+            </div>
+            <Button
+            className={confirm? styles.confirm : styles.notConfirm}
+              disabled={!confirm}
+              type="submit"
+              value="Submit"
+            >
+              Invia
+            </Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
